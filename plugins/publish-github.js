fc9b5748e9dd6f67e4f23e0f668d953528e8e5e7
Viktor Varland <viktor@dhis2.org>
2022-01-27 14:11:05 +0100

    feat(assets): publish the app bundle in github release

--- plugins/publish-github.js
+++ plugins/publish-github.js
@@ -4,4 +4,14 @@ const github = require('@semantic-release/github')
  * to ensure that the semantic-release plugin is bundled into the dist
  * artifact ncc produces, we need to require it and reference it in code.
  */
-exports.githubPlugin = () => [github]
+exports.githubPlugin = () => [
+    github,
+    {
+        assets: [
+            {
+                path: 'build/bundle/*.zip',
+                label: 'DHIS2 compatible application archive',
+            },
+        ],
+    },
+]
