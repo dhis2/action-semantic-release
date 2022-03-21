9e3b95192e27bfb9d9885270f754ddff195d6b16
Viktor Varland <viktor@dhis2.org>
2022-03-21 12:00:40 +0100

    fix: avoid exception when no commits

--- custom/semantic-release-defer-release.js
+++ custom/semantic-release-defer-release.js
@@ -1,13 +1,19 @@
 const analyzeCommits = (config, context) => {
     const { logger, commits } = context
 
-    const { message, commit } = commits[0]
-    const defer = /\[(defer|skip)[ -]release\]/gi
+    if (commits) {
+        const { message, commit } = commits[0]
+        const defer = /\[(defer|skip)[ -]release\]/gi
 
-    if (message.match(defer)) {
-        logger.warn(`This release has been deferred by commit ${commit.short}`)
-        logger.complete('Halting release process...')
-        process.exit(0)
+        if (message.match(defer)) {
+            logger.warn(
+                `This release has been deferred by commit ${commit.short}`
+            )
+            logger.complete('Halting release process...')
+            process.exit(0)
+        }
+    } else {
+        logger.info('No commits to analyze.')
     }
 }
 
