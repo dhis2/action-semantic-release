8056f4b672fa029e0b6773701d4fa800fb9e5d7d
Mozafar Haider <mozafar@dhis2.org>
2024-03-07 12:37:45 +0000

    fix: update semantic-release to allow publish from main branch

--- index.js
+++ index.js
@@ -62,6 +62,9 @@ const main = async () => {
         }),
         dryRun,
         ci,
+        // this adds "main" branch to the default branches from the version of semantic-release that we use
+        // default list is here: https://github.com/semantic-release/semantic-release/blob/v17.4.4/docs/usage/configuration.md#branches
+        branches: ['+([0-9])?(.{+([0-9]),x}).x', 'master', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}]
     }
 
     const config = {
