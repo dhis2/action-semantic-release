dd9e4abca7cbb8e8b0e76721899ca5a815d0713b
Jen Jones Arnesen <jennifer@dhis2.org>
2024-07-05 16:13:38 +0200

    chore: only publish to github

--- plugins/index.js
+++ plugins/index.js
@@ -2,22 +2,22 @@ const path = require('path')
 const core = require('@actions/core')
 const fs = require('fs-extra')
 const getWorkspacePackages = require('../support/getWorkspacePackages.js')
-const { changelogPlugin } = require('./changelog.js')
-const { commitAnalyzerPlugin } = require('./commit-analyzer.js')
-const { deferReleasePlugin } = require('./defer-release.js')
-const { gitPlugin } = require('./git.js')
-const { apphubPlugin } = require('./publish-apphub.js')
+// const { changelogPlugin } = require('./changelog.js')
+// const { commitAnalyzerPlugin } = require('./commit-analyzer.js')
+// const { deferReleasePlugin } = require('./defer-release.js')
+// const { gitPlugin } = require('./git.js')
+// const { apphubPlugin } = require('./publish-apphub.js')
 const { githubPlugin } = require('./publish-github.js')
-const { npmPlugin } = require('./publish-npm.js')
-const { releaseNotesPlugin } = require('./release-notes.js')
-const { updateDepsPlugin } = require('./update-deps.js')
+// const { npmPlugin } = require('./publish-npm.js')
+// const { releaseNotesPlugin } = require('./release-notes.js')
+// const { updateDepsPlugin } = require('./update-deps.js')
 
 /* order matters in the plugins array !
  *
  * first, semantic-release runs each phase, and if there are multiple
  * plugins for one phase, they are executed in order of the array.
  */
-exports.plugins = async ({ changelog, apphub, npm, github, cwd }) => {
+exports.plugins = async ({ /* changelog, apphub, npm, */ github, cwd }) => {
     const rootPackageFile = path.join(cwd, 'package.json')
 
     if (!fs.existsSync(rootPackageFile)) {
@@ -34,20 +34,20 @@ exports.plugins = async ({ changelog, apphub, npm, github, cwd }) => {
     core.endGroup()
 
     return [
-        deferReleasePlugin(),
-        commitAnalyzerPlugin(),
-        releaseNotesPlugin(),
-        updateDepsPlugin({ packages }),
-        changelogPlugin({ changelogFile: changelog }),
-        ...npmPlugin({ npmPublish: npm.publish, packages }),
-        ...(apphub.publish
-            ? apphubPlugin({
-                  channel: apphub.channel,
-                  baseUrl: apphub.baseUrl,
-                  packages,
-              })
-            : []),
-        gitPlugin({ packages }),
+        // deferReleasePlugin(),
+        // commitAnalyzerPlugin(),
+        // releaseNotesPlugin(),
+        // updateDepsPlugin({ packages }),
+        // changelogPlugin({ changelogFile: changelog }),
+        // ...npmPlugin({ npmPublish: npm.publish, packages }),
+        // ...(apphub.publish
+        //     ? apphubPlugin({
+        //           channel: apphub.channel,
+        //           baseUrl: apphub.baseUrl,
+        //           packages,
+        //       })
+        //     : []),
+        // gitPlugin({ packages }),
         ...(github.publish ? [githubPlugin()] : []),
     ]
 }
